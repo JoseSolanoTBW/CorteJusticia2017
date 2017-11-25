ab2173e3f05f843d87eb80fd6dff477bb346b50c
JoseSolano <jsolanom>
2017-11-25 02:58:55 -0600

    Adding CasosService

---
+++ src/data/CasosService.java
@@ -0,0 +1,40 @@
+package data;
+
+import Business.Caso.Caso;
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class CasosService extends Service{
+    private CallableStatement cs;
+    private ResultSet rs;
+    private Connection con;
+
+    public ArrayList<Caso> getCasosPorQuerellante(int id) throws IOException, SQLException {
+        con = getConnection();
+        ArrayList<Caso> querellanteList = new ArrayList<>();
+        Caso casos;
+
+        cs = con.prepareCall("Call get_cases_by_querellante(?)");
+        cs.setEscapeProcessing(true);
+        cs.setInt(1, id);
+        rs = cs.executeQuery();
+        
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+        int columnCount = rs.getMetaData().getColumnCount();
+        while (rs.next()) {
+            casos = new Caso(rs.getInt("numeroCaso"), rs.getString("descripcion"),
+                    rs.getDate("fechaCreacion"), rs.getInt("idEstado"),rs.getString("Estado"));
+            querellanteList.add(casos);
+        }
+
+        con.close();
+
+        return querellanteList;
+    }
+}
