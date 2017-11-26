f7ca8cfb79f605c564161bd67d9cff440054a505
JoseSolano <jsolanom>
2017-11-26 10:33:58 -0600

    Casos Service

--- src/data/CasosService.java
+++ src/data/CasosService.java
@@ -1,6 +1,7 @@
 package data;
 
 import Business.Caso.Caso;
+import Business.Caso.DetalleHistorial;
 import Business.Caso.Estado;
 import Business.Persona.Querellante;
 import java.io.IOException;
@@ -40,7 +41,7 @@ public class CasosService extends Service{
         return querellanteList;
     }
     
-     public ArrayList<Caso> getCasosPorJuez(int id) throws IOException, SQLException {
+    public ArrayList<Caso> getCasosPorJuez(int id) throws IOException, SQLException {
         con = getConnection();
         ArrayList<Caso> querellanteList = new ArrayList<>();
         Caso casos;
@@ -92,17 +93,83 @@ public class CasosService extends Service{
      }
      
      
-      public void create(Caso cas) throws SQLException, IOException {
-         con = getConnection();
-         cs = con.prepareCall("Call create_caso(?,?,?,?,?)");
-        java.sql.Date sqlDate = java.sql.Date.valueOf( LocalDate.now());
+    public void create(Caso cas) throws SQLException, IOException {
+        con = getConnection();
+        cs = con.prepareCall("Call create_caso(?,?,?,?,?)");
+        java.sql.Date sqlDate = java.sql.Date.valueOf(LocalDate.now());
         cs.setEscapeProcessing(true);
         cs.setString(1, cas.getDescripcion());
-       cs.setInt(2, cas.getDenunciante().getIdPersona());
+        cs.setInt(2, cas.getDenunciante().getIdPersona());
         cs.setInt(3, cas.getEstadoCaso().getIdEstado());
         cs.setDate(4, sqlDate);
-         cs.setInt(5, cas.getJuezAsignado().getNumeroJuez());
+        cs.setInt(5, cas.getJuezAsignado().getNumeroJuez());
         cs.execute();
         con.close();
     }
+      
+     public void actualizarEstado(int idCaso, int nuevoEstado) throws SQLException, IOException
+     {
+        con = getConnection();
+        cs = con.prepareCall("Call update_estado_caso(?,?)");
+        cs.setEscapeProcessing(true);
+        cs.setInt(1, idCaso);
+        cs.setInt(2, nuevoEstado);
+        cs.execute();
+        con.close();
+     } 
+     
+    public Caso getCasos(int id) throws IOException, SQLException {
+        con = getConnection();
+        Caso casos;
+
+        cs = con.prepareCall("Call get_cases_by_id(?)");
+        cs.setEscapeProcessing(true);
+        cs.setInt(1, id);
+        rs = cs.executeQuery();
+        
+        int columnCount = rs.getMetaData().getColumnCount();
+        rs.next();
+            casos = new Caso(rs.getInt("numeroCaso"), rs.getString("descripcion"),
+                    rs.getDate("fechaCreacion"), rs.getInt("idEstado"),rs.getString("Estado"),rs.getInt("Persona_idPersona"),
+                    rs.getString("QuerellateNombre"), rs.getString("apellido"));
+        
+        con.close();
+
+        return casos;
+    }
+    
+        public void createHistorial(int idEstado, int idCaso) throws SQLException, IOException {
+        con = getConnection();
+        cs = con.prepareCall("Call create_detalle_caso(?,?,?)");
+        java.sql.Date sqlDate = java.sql.Date.valueOf(LocalDate.now());
+        cs.setEscapeProcessing(true);
+        cs.setDate(1, sqlDate);
+        cs.setInt(2,idEstado);
+        cs.setInt(3, idCaso);
+        cs.execute();
+        con.close();
+    }
+        
+       public ArrayList<DetalleHistorial> getCasoId(int idCaso) throws IOException, SQLException {
+         con = getConnection();
+         ArrayList<DetalleHistorial> listaCasos = new ArrayList<>();
+         DetalleHistorial nCaso;
+ 
+         cs = con.prepareCall("Call get_histrial_by_Caso(?)");
+         cs.setEscapeProcessing(true);
+         cs.setInt(1, idCaso);
+         rs = cs.executeQuery();
+ 
+         while (rs.next()) {
+            nCaso = new DetalleHistorial();
+            nCaso.setEstadoHistorial(new Estado());
+            nCaso.getEstadoHistorial().setNombre(rs.getString("nombre"));
+            nCaso.setFechaActualizacion(rs.getDate("fechaModificacion"));
+             listaCasos.add(nCaso);
+         }
+ 
+         con.close();
+         return listaCasos;
+ 
+     }
 }
