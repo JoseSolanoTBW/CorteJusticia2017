0946c302b05eeb89dd9d28c87305812272cbdaad
Adrian Obando <cobandol@ucenfotec.ac.cr>
2017-11-25 18:33:29 -0800

    Casos

--- src/data/CasosService.java
+++ src/data/CasosService.java
@@ -1,6 +1,8 @@
 package data;
 
 import Business.Caso.Caso;
+import Business.Caso.Estado;
+import Business.Persona.Querellante;
 import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
@@ -8,6 +10,7 @@ import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class CasosService extends Service{
@@ -58,5 +61,48 @@ public class CasosService extends Service{
         con.close();
 
         return querellanteList;
+    }
+       public ArrayList<Caso> getCasosNull() throws IOException, SQLException {
+         con = getConnection();
+         ArrayList<Caso> listaCasos = new ArrayList<>();
+         Caso nCaso;
+ 
+         cs = con.prepareCall("Call get_casosNull");
+         cs.setEscapeProcessing(true);
+ 
+         rs = cs.executeQuery();
+ 
+         while (rs.next()) {
+            nCaso = new Caso();
+             Querellante quere = new Querellante();
+             Estado est = new Estado();
+             est.setNombre(rs.getString("estado"));
+             quere.setNombre(rs.getString("querellante"));
+             nCaso.setNumeroCaso(rs.getInt("numeroCaso"));
+             nCaso.setDescripcion(rs.getString("descripcion"));
+             nCaso.setFechaCfeacion(rs.getDate("fechaCreacion"));
+             nCaso.setDenunciante(quere);
+             nCaso.setEstadoCaso(est);
+             listaCasos.add(nCaso);
+         }
+ 
+         con.close();
+         return listaCasos;
+ 
+     }
+     
+     
+      public void create(Caso cas) throws SQLException, IOException {
+         con = getConnection();
+         cs = con.prepareCall("Call create_caso(?,?,?,?,?)");
+        java.sql.Date sqlDate = java.sql.Date.valueOf( LocalDate.now());
+        cs.setEscapeProcessing(true);
+        cs.setString(1, cas.getDescripcion());
+       cs.setInt(2, cas.getDenunciante().getIdPersona());
+        cs.setInt(3, cas.getEstadoCaso().getIdEstado());
+        cs.setDate(4, sqlDate);
+         cs.setInt(5, cas.getJuezAsignado().getNumeroJuez());
+        cs.execute();
+        con.close();
     }
 }
